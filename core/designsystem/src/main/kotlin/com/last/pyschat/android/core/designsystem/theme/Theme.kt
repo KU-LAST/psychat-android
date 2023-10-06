4195b58592212ac663f8858d2e928a36993db455
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-10-07 02:18:51 +0900

    Theme setup

--- core/designsystem/src/main/kotlin/com/last/pyschat/android/core/designsystem/theme/Theme.kt
+++ core/designsystem/src/main/kotlin/com/last/pyschat/android/core/designsystem/theme/Theme.kt
@@ -1,4 +1,4 @@
-package com.last.psychat.android.ui.theme
+package com.last.pyschat.android.core.designsystem.theme
 
 import android.app.Activity
 import android.os.Build
@@ -10,10 +10,18 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
+import com.nexters.bandalart.android.core.designsystem.theme.Pink40
+import com.nexters.bandalart.android.core.designsystem.theme.Pink80
+import com.nexters.bandalart.android.core.designsystem.theme.Purple40
+import com.nexters.bandalart.android.core.designsystem.theme.Purple80
+import com.nexters.bandalart.android.core.designsystem.theme.PurpleGrey40
+import com.nexters.bandalart.android.core.designsystem.theme.PurpleGrey80
+import com.nexters.bandalart.android.core.designsystem.theme.Typography
 
 private val DarkColorScheme = darkColorScheme(
   primary = Purple80,
@@ -25,20 +33,19 @@ private val LightColorScheme = lightColorScheme(
   primary = Purple40,
   secondary = PurpleGrey40,
   tertiary = Pink40,
-  /*
-   * Other default colors to override
-   * background = Color(0xFFFFFBFE),
-   * surface = Color(0xFFFFFBFE),
-   * onPrimary = Color.White,
-   * onSecondary = Color.White,
-   * onTertiary = Color.White,
-   * onBackground = Color(0xFF1C1B1F),
-   * onSurface = Color(0xFF1C1B1F),
-   */
+
+//  // Other default colors to override
+//  background = Color(0xFFFFFBFE),
+//  surface = Color(0xFFFFFBFE),
+//  onPrimary = Color.White,
+//  onSecondary = Color.White,
+//  onTertiary = Color.White,
+//  onBackground = Color(0xFF1C1B1F),
+//  onSurface = Color(0xFF1C1B1F),
 )
 
 @Composable
-fun PsychatandroidTheme(
+fun PsychatTheme(
   darkTheme: Boolean = isSystemInDarkTheme(),
   // Dynamic color is available on Android 12+
   dynamicColor: Boolean = true,
@@ -49,7 +56,6 @@ fun PsychatandroidTheme(
       val context = LocalContext.current
       if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
     }
-
     darkTheme -> DarkColorScheme
     else -> LightColorScheme
   }
@@ -60,6 +66,24 @@ fun PsychatandroidTheme(
       window.statusBarColor = colorScheme.primary.toArgb()
       WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
     }
+  }
+
+  SideEffect {
+    val window = (view.context as Activity).window
+
+    window.statusBarColor = if (darkTheme) Color.Black.toArgb() else Color.White.toArgb()
+    window.navigationBarColor = if (darkTheme) Color.Black.toArgb() else Color.White.toArgb()
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+      // remove unnecessary black screen from bars
+      window.isNavigationBarContrastEnforced = false
+    }
+
+    val windowsInsetsController = WindowCompat.getInsetsController(window, view)
+
+    // status bar's icon always visible
+    windowsInsetsController.isAppearanceLightStatusBars = !darkTheme
+    windowsInsetsController.isAppearanceLightNavigationBars = !darkTheme
   }
 
   MaterialTheme(
