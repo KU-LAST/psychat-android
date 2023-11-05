0e31085fcf565aa3475d447c8f9f18de9b20af56
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-11-05 19:47:15 +0900

    채팅 화면 키보드 개행 허용, 메세지 보내기 버튼 추가

--- feature/chat/src/main/kotlin/com/last/psychat/android/feature/chat/ChatScreen.kt
+++ feature/chat/src/main/kotlin/com/last/psychat/android/feature/chat/ChatScreen.kt
@@ -3,15 +3,18 @@ package com.last.psychat.android.feature.chat
 import android.widget.Toast
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.Send
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
@@ -20,7 +23,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -127,30 +130,35 @@ internal fun ChatScreen(
               ChatBubble(chatMessage = chatMessageList[index])
             }
           }
-
-          OutlinedTextField(
+          Row(
             modifier = Modifier
-              .fillMaxWidth()
-              .align(Alignment.BottomCenter)
-              .padding(
-                start = 16.dp,
-                end = 16.dp,
-                bottom = 32.dp,
+              .padding(bottom = 32.dp)
+              .align(Alignment.BottomCenter),
+          ) {
+            OutlinedTextField(
+              modifier = Modifier
+                .padding(start = 16.dp)
+                .weight(1f),
+              value = uiState.chatInputMessage,
+              singleLine = false,
+              onValueChange = updateChatInputMessage,
+              keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text
               ),
-            value = uiState.chatInputMessage,
-            singleLine = true,
-            onValueChange = updateChatInputMessage,
-            keyboardOptions = KeyboardOptions(
-              imeAction = ImeAction.Done
-            ),
-            keyboardActions = KeyboardActions(
-              onDone = {
+            )
+            IconButton(
+              onClick = {
                 if (uiState.chatInputMessage.isNotEmpty()) {
                   sendChatMessage()
                 }
               }
-            )
-          )
+            ) {
+              Icon(
+                imageVector = Icons.AutoMirrored.Outlined.Send,
+                contentDescription = "Send Message",
+              )
+            }
+          }
         }
       }
     }
