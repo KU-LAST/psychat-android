65d2f4aea562254341ca293464fa783ffba625c9
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-11-09 15:11:10 +0900

    TODO 추가

--- feature/main/src/main/kotlin/com/last/psychat/android/feature/mapper/PreviousChatMapper.kt
+++ feature/main/src/main/kotlin/com/last/psychat/android/feature/mapper/PreviousChatMapper.kt
@@ -3,6 +3,7 @@ package com.last.psychat.android.feature.mapper
 import com.last.psychat.android.core.domain.entity.chat.PreviousChatEntity
 import com.last.psychat.android.feature.model.PreviousChat
 
+// TODO 아직 진행 중인 대화일 경우 emotion 이 보이지 않도록
 internal fun PreviousChatEntity.toUiModel() =
   PreviousChat(
     sessionId = sessionId,
