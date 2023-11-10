5d7d1c8f2f8815cfbbcda0a76d59aa8c8f7ef772
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-11-10 16:44:46 +0900

    더미 데이터 양식 변경

--- feature/chat/src/main/kotlin/com/last/psychat/android/feature/chat/ChatViewModel.kt
+++ feature/chat/src/main/kotlin/com/last/psychat/android/feature/chat/ChatViewModel.kt
@@ -57,44 +57,44 @@ class ChatViewModel @Inject constructor(
   private val _eventFlow = MutableSharedFlow<ChatUiEvent>()
   val eventFlow: SharedFlow<ChatUiEvent> = _eventFlow.asSharedFlow()
 
-//  init {
-//    viewModelScope.launch {
-//      _uiState.update {
-//        it.copy(chatMessageList = listOf(
-//          ChatMessage(
-//            message = "나 요즘 너무 힘들어",
-//            timestamp = "오전 10시 12분",
-//            isUser = true,
-//          ),
-//          ChatMessage(
-//            message = "네 그렇군요, 듣고 있으니 계속 말씀하세요",
-//            timestamp = "오전 10시 13분",
-//            isUser = false,
-//          ),
-//          ChatMessage(
-//            message = "학교 팀플에 취업 준비에 너무 할게 많아서 스트레스 받아. 그냥 좀 쉬고 싶어",
-//            timestamp = "오전 10시 14분",
-//            isUser = true,
-//          ),
-//          ChatMessage(
-//            message = "그런 일이 있으셨군요. 정말 힘드시겠어요. 제가 옆에서 힘이 되어 드릴께요",
-//            timestamp = "오전 10시 15분",
-//            isUser = false,
-//          ),
-//          ChatMessage(
-//            message = "어떻게 스트레스를 그나마 조금이라도 해소할 수 있을까?",
-//            timestamp = "오전 10시 16분",
-//            isUser = true,
-//          ),
-//          ChatMessage(
-//            message = "쉬는 시간에 가볍게 산책을 다녀오시는 건 어떨까요?",
-//            timestamp = "오전 10시 17분",
-//            isUser = false,
-//          ),
-//        ))
-//      }
-//    }
-//  }
+  init {
+    viewModelScope.launch {
+      _uiState.update {
+        it.copy(chatMessageList = listOf(
+          ChatMessageUiModel(
+            message = "나 요즘 너무 힘들어",
+            timestamp = "오전 10시 12분",
+            isUser = true,
+          ),
+          ChatMessageUiModel(
+            message = "네 그렇군요, 듣고 있으니 계속 말씀하세요",
+            timestamp = "오전 10시 13분",
+            isUser = false,
+          ),
+          ChatMessageUiModel(
+            message = "학교 팀플에 취업 준비에 너무 할게 많아서 스트레스 받아. 그냥 좀 쉬고 싶어",
+            timestamp = "오전 10시 14분",
+            isUser = true,
+          ),
+          ChatMessageUiModel(
+            message = "그런 일이 있으셨군요. 정말 힘드시겠어요. 제가 옆에서 힘이 되어 드릴께요",
+            timestamp = "오전 10시 15분",
+            isUser = false,
+          ),
+          ChatMessageUiModel(
+            message = "어떻게 스트레스를 그나마 조금이라도 해소할 수 있을까?",
+            timestamp = "오전 10시 16분",
+            isUser = true,
+          ),
+          ChatMessageUiModel(
+            message = "쉬는 시간에 가볍게 산책을 다녀오시는 건 어떨까요?",
+            timestamp = "오전 10시 17분",
+            isUser = false,
+          ),
+        ))
+      }
+    }
+  }
 
   init {
     getPreviousChatDetail(sessionId)
