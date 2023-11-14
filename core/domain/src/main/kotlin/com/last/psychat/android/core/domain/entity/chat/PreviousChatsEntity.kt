b686f0684480ff3d4f84898ac6a2383e927fc180
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-11-14 14:41:11 +0900

    stable marker 라이브러리 적용

--- core/domain/src/main/kotlin/com/last/psychat/android/core/domain/entity/chat/PreviousChatsEntity.kt
+++ core/domain/src/main/kotlin/com/last/psychat/android/core/domain/entity/chat/PreviousChatsEntity.kt
@@ -1,9 +1,12 @@
 package com.last.psychat.android.core.domain.entity.chat
 
+import androidx.compose.runtime.Stable
+
 data class PreviousChatListEntity(
   val previousChatList: List<PreviousChatEntity>,
 )
 
+@Stable
 data class PreviousChatEntity(
   val sessionId: Long,
   val startDate: String,
--- core/domain/src/main/kotlin/com/last/psychat/android/core/domain/entity/recommend/RecommendedContentListEntity.kt
+++ core/domain/src/main/kotlin/com/last/psychat/android/core/domain/entity/recommend/RecommendedContentListEntity.kt
@@ -1,9 +1,12 @@
 package com.last.psychat.android.core.domain.entity.recommend
 
+import androidx.compose.runtime.Stable
+
 data class RecommendedContentListEntity(
   val contentsList: List<RecommendedContentEntity>,
 )
 
+@Stable
 data class RecommendedContentEntity(
   val date: String,
   val title: String,
