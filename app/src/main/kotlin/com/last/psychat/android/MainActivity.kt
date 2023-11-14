44a49923cd492b3d9fed0f625c22e3f1b7e63fbf
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-11-14 16:13:05 +0900

    WindowInsets 활성화

--- app/src/main/kotlin/com/last/psychat/android/MainActivity.kt
+++ app/src/main/kotlin/com/last/psychat/android/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowCompat
 import com.last.psychat.android.ui.PsyChatApp
 import com.last.pyschat.android.core.designsystem.theme.PsychatTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -13,6 +14,9 @@ class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     installSplashScreen()
+
+    WindowCompat.setDecorFitsSystemWindows(window, false)
+
     setContent {
       PsychatTheme {
         PsyChatApp()
--- feature/chat/src/main/kotlin/com/last/psychat/android/feature/chat/ChatScreen.kt
+++ feature/chat/src/main/kotlin/com/last/psychat/android/feature/chat/ChatScreen.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -123,7 +122,7 @@ internal fun ChatScreen(
         ) {
           LazyColumn(
             modifier = Modifier
-              .fillMaxHeight()
+              .fillMaxSize()
               .padding(bottom = 120.dp),
             state = listState
           ) {
